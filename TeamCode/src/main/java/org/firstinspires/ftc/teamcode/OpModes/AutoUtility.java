17a03a4607542e4aa686f2281ab867a8fc87c02f
Aarav Sharma <iamcoder18@gmail.com>
2024-11-03 16:42:48 -0700

    April tag changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutoUtility.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import android.util.Size;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+public class AutoUtility {
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    Robot robot;
+    Telemetry telemetry;
+    HardwareMap hardwareMap;
+    public AutoUtility (HardwareMap hardwareMap, Telemetry telemetry){
+        this.telemetry = telemetry;
+        this.hardwareMap = hardwareMap;
+
+        robot = new Robot(hardwareMap, telemetry, true);
+
+        Constants.addAprilTags();
+
+        aprilTag = robot.aprilTag;
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera
+        builder.setCamera(robot.webcam);
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(Constants.Camera.width, Constants.Camera.height));
+
+        builder.enableLiveView(false);
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        // builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether LiveView stops if no processors are enabled.
+        builder.setAutoStopLiveView(true);
+
+        // Set and enable the processor.
+        builder.addProcessor(robot.aprilTag);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        visionPortal.setProcessorEnabled(robot.aprilTag, true);
+
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+    }
+
+    public void loop(){
+        telemetryAprilTag();
+
+        telemetry.update();
+    }
+
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
+                        detection.robotPose.getPosition().x,
+                        detection.robotPose.getPosition().y,
+                        detection.robotPose.getPosition().z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
+                        detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
+                        detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
+                        detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+
+    }
+}
