907d8e936e213c60574ed0ea6799f72daf5f854e
Aarav Sharma <iamcoder18@gmail.com>
2024-11-10 14:00:38 -0700

    Added limits to LiftAngleMotorV2
    Added forward and backward limits to the LiftAngleMotorV2 class to prevent the motor from exceeding its intended range of motion.
    The motor is now initialized in RUN_USING_ENCODER mode.
    The Move method now checks if the target position is within the limits before setting the motor power.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LiftAngleMotorV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LiftAngleMotorV2.java
@@ -6,9 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class LiftAngleMotorV2 {
     public DcMotor liftAngleMotor;
     public double location;
+    public int forwardLimit = 900;
+    public int backwardLimit = -500;
 
     public LiftAngleMotorV2(DcMotor liftAngleMotor) {
         this.liftAngleMotor = liftAngleMotor;
+        this.liftAngleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         this.location = liftAngleMotor.getCurrentPosition();
     }
 
@@ -20,8 +23,11 @@ public class LiftAngleMotorV2 {
     }
 
     public void Move(double speed) {
-        liftAngleMotor.setPower(1);
-        liftAngleMotor.setTargetPosition((int) (location + speed));
-        liftAngleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (!((location + speed) > forwardLimit) && !((location + speed) < backwardLimit)) {
+            liftAngleMotor.setTargetPosition((int) (location + speed));
+            liftAngleMotor.setPower(1);
+        } else {
+            liftAngleMotor.setPower(0);
+        }
     }
 }
