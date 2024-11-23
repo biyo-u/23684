662d5ebce9e73093b81c5f34f8ac77a38596c79b
jayden <juvbiama@icloud.com>
2024-11-22 20:27:51 -0700

    more lines  created, hard coded auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardcodeauto/movements/Lines.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardcodeauto/movements/Lines.java
@@ -11,8 +11,29 @@ public class Lines {
 
     public static void MoveX(double power){
         robot.drive.setFrontLeftSpeed(power);
-        robot.drive.setFrontLeftSpeed(power);
-        robot.drive.setFrontLeftSpeed(power);
-        robot.drive.setFrontLeftSpeed(power);
+        robot.drive.setRearRightSpeed(power);
+        robot.drive.setFrontRightSpeed(power);
+        robot.drive.setRearLeftSpeed(power);
+    }
+
+    public static void MoveY(double power){
+        robot.drive.setFrontLeftSpeed(-power);
+        robot.drive.setRearRightSpeed(-power);
+        robot.drive.setFrontRightSpeed(power);
+        robot.drive.setRearLeftSpeed(power);
+    }
+    public static void heading(double power){
+        robot.drive.setFrontLeftSpeed(-power);
+        robot.drive.setRearRightSpeed(power);
+        robot.drive.setFrontRightSpeed(-power);
+        robot.drive.setRearLeftSpeed(power);
+    }
+
+    public static void Movement(double x ,double y , double turning, double power){
+        robot.drive.setFrontLeftSpeed(((y - x - turning) / 1 ) / power);
+        robot.drive.setRearRightSpeed();
+        robot.drive.setFrontRightSpeed();
+        robot.drive.setRearLeftSpeed(((y + x - turning) / 1) / power;);
+
     }
 }
