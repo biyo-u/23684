82314016e7e302126ecc692e59fdff8bbf109104
biyo-u <biyouvbiama@gmail.com>
2024-11-22 19:45:24 -0700

    HARDCODED AUTONOMOUS TESTING!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardcodeauto/BlueAuto.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.hardcodeauto;
+
+public class BlueAuto {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardcodeauto/movements/Lines.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.hardcodeauto.movements;
+
+public class Lines {
+}
