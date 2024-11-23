dcb9c34d7f8acf51487ff9e85e90001b119333ef
biyo-u <biyouvbiama@gmail.com>
2024-11-22 21:04:54 -0700

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lines.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lines.java
@@ -12,22 +12,22 @@ public class Lines {
 
     public static void MoveX(double power){
         robot.drive.getFrontLeft().setPower(power);
-        robot.drive.setRearRightSpeed(power);
-        robot.drive.setFrontRightSpeed(power);
-        robot.drive.setRearLeftSpeed(power);
+        robot.drive.getRearRight().setPower(power);
+        robot.drive.getFrontRight().setPower(power);
+        robot.drive.getRearLeft().setPower(power);
     }
 
     public static void MoveY(double power){
-        robot.drive.setFrontLeftSpeed(-power);
-        robot.drive.setRearRightSpeed(-power);
-        robot.drive.setFrontRightSpeed(power);
-        robot.drive.setRearLeftSpeed(power);
+        robot.drive.getFrontLeft().setPower(-power);
+        robot.drive.getRearRight().setPower(-power);
+        robot.drive.getFrontLeft().setPower(power);
+        robot.drive.getRearLeft().setPower(power);
     }
     public static void heading(double power){
-        robot.drive.setFrontLeftSpeed(-power);
-        robot.drive.setRearRightSpeed(power);
-        robot.drive.setFrontRightSpeed(-power);
-        robot.drive.setRearLeftSpeed(power);
+        robot.drive.getFrontLeft().setPower(-power);
+        robot.drive.getRearRight().setPower(power);
+        robot.drive.getFrontLeft().setPower(-power);
+        robot.drive.getRearLeft().setPower(power);
     }
 
     public static void Movement(double x ,double y , double turning, double power){
