36da7353c4c495117e7f4eb437ae017206289a42
jayden <juvbiama@icloud.com>
2024-11-22 20:11:42 -0700

    lines created, hard coded auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardcodeauto/movements/Lines.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardcodeauto/movements/Lines.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.hardcodeauto.movements;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.Robot;
+
+
 public class Lines {
+
+ public static Robot robot;
+
+
+    public static void MoveX(double power){
+        robot.drive.setFrontLeftSpeed(power);
+        robot.drive.setFrontLeftSpeed(power);
+        robot.drive.setFrontLeftSpeed(power);
+        robot.drive.setFrontLeftSpeed(power);
+    }
 }
