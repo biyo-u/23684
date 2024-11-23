26b614b32ee34f13f5b6397c8acd81120276689b
Aarav Sharma <iamcoder18@gmail.com>
2024-11-23 13:00:25 -0700

    Removed unused import from Acme.java
    Removed the unused import for `NewFile` from `Acme.java`.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Acme.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Acme.java
@@ -5,8 +5,6 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.onbotjava.handlers.file.NewFile;
 import org.firstinspires.ftc.teamcode.Roadrunner.PinpointDrive;
 import org.firstinspires.ftc.teamcode.Utilities;
 
