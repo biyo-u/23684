86ba6c85873d0e5018aed262dc163ce42c839d78
jayden <juvbiama@icloud.com>
2024-11-29 20:01:19 -0700

    hard coding

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/Old/AutoActions.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.OpModes.Auto.Old;
+import  org.firstinspires.ftc.teamcode.Robot;
+
+public class AutoActions {
+    Robot robot;
+
+public void Movement(double x, double y, double turning )
+
+}
